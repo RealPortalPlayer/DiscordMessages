6e72864274fa4f2741b04b69bafd9b565359aa2a
portalplayer <kratcy@fuckb.ing>
2025-01-29 23:44:49 -0500

    index: Removed stray brace

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const serverIndex = JSON.parse(readFileSync("package/messages/index.json").toStr
 let html = ""
 
 for (const id of Object.keys(serverIndex))
-    html += `<a href="/${id}.html">${serverIndex[id]} ({${id})</a><br>`
+    html += `<a href="/${id}.html">${serverIndex[id]} (${id})</a><br>`
 
 writeFileSync("parsed/index.html", html)
 
