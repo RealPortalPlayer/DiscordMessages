b2cc7cf898b026fbbdf300ae01be61bf473f0e68
portalplayer <kratcy@fuckb.ing>
2025-01-29 23:39:17 -0500

    index: Parse server messages

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const {readFileSync, writeFileSync} = require("fs")
+const {readFileSync, writeFileSync, readdirSync} = require("fs")
 
 console.log("Parsing server index")
 
@@ -10,3 +10,27 @@ for (const id of Object.keys(serverIndex))
     html += `<a href="/${id}.html">${serverIndex[id]} ({${id})</a><br>`
 
 writeFileSync("parsed/index.html", html)
+
+html = ""
+
+console.log("Parsing messages")
+
+const messages = readdirSync("package/messages")
+
+for (const id of messages) {
+    if (!id.startsWith("c"))
+        continue
+
+    const parsedId = id.substring(1)
+
+    console.log(`Doing: ${serverIndex[parsedId]} (${parsedId})`)
+
+    const messages = JSON.parse(readFileSync(`package/messages/${id}/messages.json`).toString())
+
+    for (const message of messages)
+        html += `[${message.ID} @ ${message.Timestamp}]: ${message.Contents}<br>` // TODO: attachments?
+
+    writeFileSync(`parsed/${parsedId}.html`, html)
+
+    html = ""
+}
